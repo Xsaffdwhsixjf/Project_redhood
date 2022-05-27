441bb1c1a11380c62dd8a2cef0aa8d66c2f53338
JyunCorner <jyun119119@gmail.com>
2022-05-27 17:01:18 +0800

    0527RWD

--- 專題/程式工作區/js/hotel.js
+++ 專題/程式工作區/js/hotel.js
@@ -1,4 +1,6 @@
+let screen_width = document.querySelector('html').clientWidth
 let card = document.querySelector('#hotel_info');
+let top_btn =document.querySelector('.top_btn')
 fetch('../js/ball_hotel.json')
     .then(function (response) {
         return response.json();
@@ -7,6 +9,7 @@ fetch('../js/ball_hotel.json')
         console.log(index);
         print(index);
     });
+console.log(screen_width);
 
 function print(index) {
     let N = 149;
@@ -33,8 +36,8 @@ function print(index) {
         };
         let A1;
         let A2;
-if(web1==''){
-     A1 = `<td>
+        if (web1 == '') {
+            A1 = `<td>
     <div class="info_card">
     <div class="img">
     <img src="${pic1}" alt="${pic_explain1}" ></div>
@@ -45,21 +48,21 @@ if(web1==''){
             </div>
         </div>
         </td>`
-}else{
-     A1 = `<td>
+        } else {
+            A1 = `<td>
     <div class="info_card">
     <div class="img">
     <img src="${pic1}" alt="${pic_explain1}" ></div>
     <div class="content">
-    <a href="${web1}" target=_blank><h1>${name1}</h1></a>
+    <h1><a href="${web1}" target=_blank>${name1}</a></h1>
     <p><i class="fa-solid fa-location-dot"></i>${address1}</p>
                 <p><i class="fa-solid fa-phone"></i>${telephone1}</p>
             </div>
         </div>
         </td>`
-}
-if(web2==''){
-     A2 = `<td>
+        }
+        if (web2 == '') {
+            A2 = `<td>
     <div class="info_card">
     <div class="img">
     <img src="${pic2}" alt="${pic_explain2}" ></div>
@@ -70,19 +73,19 @@ if(web2==''){
             </div>
         </div>
         </td>`
-}else{
-     A2 = `<td>
+        } else {
+            A2 = `<td>
     <div class="info_card">
     <div class="img">
     <img src="${pic2}" alt="${pic_explain2}" ></div>
     <div class="content">
-    <a href="${web2}" target=_blank><h1>${name2}</h1></a>
+    <h1><a href="${web2}" target=_blank>${name2}</a></h1>
     <p><i class="fa-solid fa-location-dot"></i>${address2}</p>
                 <p><i class="fa-solid fa-phone"></i>${telephone2}</p>
             </div>
         </div>
         </td>`
-}
+        }
         card.innerHTML += `<tr>${A1}${A2}</tr>`;
         // if (i == N - 1 && (N % 2 != 0)) {
         //     card.innerHTML += `<tr>${A1}<td></td></tr>`;
@@ -90,6 +93,8 @@ if(web2==''){
         //     card.innerHTML += `<tr>${A1}${A2}</tr>`;
         // }
     };
+    if(screen_width>1250){
+        
     $(document).ready(function () {
         $('#table_id').dataTable({
             "bPaginate": true,
@@ -108,4 +113,24 @@ if(web2==''){
             }
         });
     });
-}
+}else{
+    top_btn.classList.remove('d-none')
+    $(document).ready(function () {
+        $('#table_id').dataTable({
+            "bPaginate": false,
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "lengthMenu": [3],
+            "bAutoWidth": false,
+            "language": {
+                "emptyTable": "資料載入時，發生了錯誤，試試重新整理",
+                "zeroRecords": "無搜尋結果，試試更換關鍵字",
+                "oPaginate": {
+                    "sNext": '<i class="fa-solid fa-angles-right"></i>',
+                    "sPrevious": '<i class="fa-solid fa-angles-left"></i>'
+                }
+            }
+        });
+    });
+}}
