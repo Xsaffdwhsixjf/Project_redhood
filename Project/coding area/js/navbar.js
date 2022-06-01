c171eb16472c02b4675c0aa6d71378bd96e6edfb
Lucky-Jan <82651206+Lucky-Jan@users.noreply.github.com>
2022-06-01 19:44:50 +0800

    水上遊玩, 特色小店, 特色小食 - Complete

--- Project/coding area/js/navbar.js
+++ Project/coding area/js/navbar.js
@@ -1,33 +1,34 @@
-const btn1 = document.querySelector('.btn1')
-const btn2 = document.querySelector('.btn2')
-const list1 = document.querySelector('.list1')
-const list2 = document.querySelector('.list2')
-btn1.addEventListener('click', () => {
-    list1.classList.toggle('show')
-})
-btn2.addEventListener('click', () => {
-    list2.classList.toggle('show')
-})
+    // Navbar下拉清單js
+    const btn1 = document.querySelector('.btn1')
+    const btn2 = document.querySelector('.btn2')
+    const list1 = document.querySelector('.list1')
+    const list2 = document.querySelector('.list2')
+    btn1.addEventListener('click', () => {
+        list1.classList.toggle('show')
+    })
+    btn2.addEventListener('click', () => {
+        list2.classList.toggle('show')
+    })
 
-// 最新消息橋段
-let news_btn = document.querySelector('.news')
-let news = document.querySelector('#news')
-let turtle = document.querySelector('.turtle');
-let whale = document.querySelector('.whale');
+    // 最新消息橋段
+    let news_btn = document.querySelector('.news')
+    let news = document.querySelector('#news')
+    let turtle = document.querySelector('.turtle');
+    let whale = document.querySelector('.whale');
 
 
-whale.addEventListener('click', function () {
-    whale.classList.toggle('d-none');
-    turtle.classList.toggle('d-none');
-    news.classList.toggle('open');
-})
+    whale.addEventListener('click', function () {
+        whale.classList.toggle('d-none');
+        turtle.classList.toggle('d-none');
+        news.classList.toggle('open');
+    })
 
-turtle.addEventListener('click', function () {
-    whale.classList.toggle('d-none');
-    turtle.classList.toggle('d-none');
-    news.classList.toggle('open');
-})
-news_btn.addEventListener('click', function () {
-    news.classList.toggle('open');
-    event.preventDefault();
-})
+    turtle.addEventListener('click', function () {
+        whale.classList.toggle('d-none');
+        turtle.classList.toggle('d-none');
+        news.classList.toggle('open');
+    })
+    news_btn.addEventListener('click', function () {
+        news.classList.toggle('open');
+        event.preventDefault();
+    })
